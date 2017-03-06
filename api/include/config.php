23c884ebc48c33764d482277e84e87ad23cdeedf
Kapil <genius.kapil@gmail.com>
2017-03-06 12:08:45 +0530

    committing database name change.

--- api/include/config.php
+++ api/include/config.php
@@ -6,5 +6,5 @@
 define("DB_HOST", "localhost"); //write your host
 define("DB_USER", "root"); //write your db username
 define("DB_PASSWORD", ""); //write your db password
-define("DB_DATABASE", "php_login_webservice"); //write your database name
+define("DB_DATABASE", "image_footage"); //write your database name
 ?>
