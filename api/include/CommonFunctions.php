2a5dad0b7ab9ade6c69315b2b395690b6912070f
pahalsandeep <pahalsandeep@gmail.com>
2017-03-21 12:38:26 +0530

    Updating api files

---
+++ api/include/CommonFunctions.php
@@ -0,0 +1,163 @@
+<?php
+$cf = new Common_Functions();
+// json response array
+$response = array("error" => FALSE);
+
+$request_body = file_get_contents('php://input');
+
+$data = json_decode($request_body);
+$task=(!empty($data->task)?$data->task:"list");
+$data->username="adad";
+if (isset($data->username)) {
+ 
+    // receiving the post params
+    $email = $data->username;
+    //$password = base64_decode($data->password);
+	
+    // get the user by username
+    $user = $cf->isUserExistedByUsername($email);
+    if ($user != false){
+            
+	  
+    } 
+	else
+	{
+        // user is not found with the credentials
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Login credentials are wrong. Please try again!";
+        echo json_encode($response);
+		exit;
+    }
+} else {
+    // required post params is missing
+    $response["error"] = TRUE;
+    $response["error_msg"] = "You are not authrize to access!";
+    echo json_encode($response);
+	exit;
+}
+
+
+
+
+
+
+
+
+class Common_Functions {
+ 
+    protected $conn;
+ 
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $db = new Db_Connect();
+        $this->conn = $db->connect();
+    }
+ 
+    // destructor
+    function __destruct() {
+         
+    }
+ 
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($name, $email, $password) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+ 
+        $stmt = $this->conn->prepare("INSERT INTO users(unique_id, name, email, encrypted_password, salt, created_at) VALUES(?, ?, ?, ?, ?, NOW())");
+        $stmt->bind_param("sssss", $uuid, $name, $email, $encrypted_password, $salt);
+        $result = $stmt->execute();
+        $stmt->close();
+ 
+        // check for successful store
+        if ($result) {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+ 
+            return $user;
+        } else {
+            return false;
+        }
+    }
+ 
+    /**
+     * Get user by email and password
+     */
+    public function getUserByEmailAndPassword($email, $password) {
+ 
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+ 
+        $stmt->bind_param("s", $email);
+ 
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+            // check for password equality
+            if ($user['password'] == md5($password)) {
+                // user authentication details are correct
+                return $user;
+            }
+        } else {
+            return NULL;
+        }
+    }
+ 
+    /**
+     * Check user is existed or not
+     */
+    public function isUserExisted($email) {
+        $stmt = $this->conn->prepare("SELECT email from users WHERE email = ?");
+ 
+        $stmt->bind_param("s", $email);
+ 
+        $stmt->execute();
+ 
+        $stmt->store_result();
+ 
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+	
+	/**
+     * Check user is existed or not by username
+     */
+    public function isUserExistedByUsername($username) {
+        $stmt = $this->conn->prepare("SELECT username from users WHERE username = ?");
+		
+		$stmt->bind_param("s", $username);
+ 
+        $stmt->execute();
+ 
+        $stmt->store_result();
+ 
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+}
+
+
+ 
+?>
---
+++ api/transactions.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'include/CommonFunctions.php';
+        
+class Transactions_Functions extends Common_Functions{
+ 
+    /**
+		 * Get transactions
+	 */
+	function getTransactions() { 
+	 
+			$stmt = $this->conn->prepare("SELECT a.id, a.txn_id, a.order_total, a.order_date, a.order_status,b.username  FROM orders as a left join users as b on a.user_id=b.id");
+	            
+			if ($stmt->execute()) {
+				$result = $stmt->get_result()->fetch_assoc();
+				$stmt->close();
+				return $result;
+				
+			} else {
+				return NULL;
+			}
+	}
+} 
+
+$tf = new Transactions_Functions();
+if($task=="list"){
+			
+			$result_data=$tf->getTransactions();
+			$response["error"] = FALSE;
+			$response["error_msg"] = "";
+			$response["data"] = $result_data;
+			echo json_encode($response);
+}
+?>
